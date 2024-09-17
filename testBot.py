d8432579f1a8852a3163a8cac7509e080228a1d5
Victor Chen <chiangochen@gmail.com>
2024-09-17 03:27:33 +0000

    init bot

---
+++ testBot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from config import BOT_TOKEN
+
+# Create a bot with default intents
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
+    print("Commands synced")
+
+# Define a command that responds with "Hello, World!"
+@bot.tree.command(name='hello', description="Replies")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello, World! {interaction.user.mention}")
+
+bot.run(BOT_TOKEN)
+
