6f3e2c2c061c18ab4581f520da41b10809b5c577
Chiango Chen <chiangochen@gmail.com>
2024-09-19 02:04:54 -0700

    crontab

--- googleCred.py
+++ googleCred.py
@@ -2,7 +2,6 @@ from config import SERVICE_ACCOUNT_CREDENTIALS
 from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
-SERVICE_ACCOUNT_FILE = SERVICE_ACCOUNT_CREDENTIALS
 
 credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        SERVICE_ACCOUNT_CREDENTIALS, scopes=SCOPES)
