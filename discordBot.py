613441a87a385b9eaca0346678cf5289088f4bd0
Chiango Chen <chiangochen@gmail.com>
2024-09-19 02:13:59 -0700

    fixing crontjobs

--- discordBot.py
+++ discordBot.py
@@ -15,8 +15,8 @@ def sendDiscordMessage(message):
 
 def createFormMessage():
     formId = getFormId()
-    message = f"New FredderLoop issue just dropped! 3 weeks to add questions here: https://docs.google.com/forms/d/{formId}/edit"
-    message += "Also, don't forget to set edit responses to true... google form api currently doesn't support setting it in code"
+    message = f"New FredderLoop issue just dropped! 3 weeks to add questions here: https://docs.google.com/forms/d/{formId}/edit "
+    message += "\nAlso, don't forget to set edit responses to true... google form api currently doesn't support setting it in code"
     sendDiscordMessage(message)
 
 def collectResponsesMessage():
