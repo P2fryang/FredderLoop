b5bffb2038775d372937d0a8e1ec90ee345d50f4
Chiango Chen <chiangochen@gmail.com>
2024-09-27 17:44:21 -0700

    database

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 
 def saveFormId(formId):
-    with open("database", "w") as f:
+    with open("database/formId", "w") as f:
         f.write(formId)
 
 def getFormId():
-    with open("database", "r") as f:
+    with open("database/formId", "r") as f:
         return f.read()
