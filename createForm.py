a55580e342240d9c43a808eef846b7c1c3746d7a
P2fryang <61068104+P2fryang@users.noreply.github.com>
2025-02-12 18:51:36 -0800

    update form creation, id save, and output order

--- createForm.py
+++ createForm.py
@@ -13,22 +13,25 @@ if __name__ == "__main__":
     drive_service = create_service(constants.DRIVE_SERVICE)
 
     # create the form
+    masker.log("creating form")
     form = formUtil.create_form(form_service=form_service)
     formId = form["formId"]
-
-    # add default questions
-    formUtil.add_default_form_body()
-
-    # set form editing permissions to anyone with the link
-    driveUtil.add_anyone_write(drive_service=drive_service, file_id=formId)
-
-    # Move the form to specific folderId
-    driveUtil.move_file_to_folder(
-        drive_service=drive_service, file_id=formId, folder_id=GOOGLE_DRIVE_FOLDER_ID
-    )
-
     # save formId in database
     masker.log(f"form created: {formId[0:3]}********")
     saveFormId(form["formId"])
+
+    # add default questions
+    masker.log("add default questions")
+    formUtil.add_default_form_body(form_service=form_service, form_id=formId)
+
+    # set form editing permissions to anyone with the link
+    masker.log("add editing permissions")
+    driveUtil.add_anyone_write(drive_service=drive_service, file_id=formId)
+
+    # Move the form to specific folderId
+    masker.log("moving folder")
+    driveUtil.move_file_to_folder(
+        drive_service=drive_service, file_id=formId, folder_id=GOOGLE_DRIVE_FOLDER_ID
+    )
 
     createFormMessage()
